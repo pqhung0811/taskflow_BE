46e44cf9db741564250cfb0d8528a8b1e57d50bc
Bo Hung <p.q.hung0811@gmail.com>
2024-06-06 03:30:39 +0700

    update

--- src/main/java/com/example/taskflow/controllers/StatisticsController.java
+++ src/main/java/com/example/taskflow/controllers/StatisticsController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +30,87 @@ import java.util.stream.Collectors;
 public class StatisticsController {
     @Autowired
     private TaskService taskService;
+
+    @GetMapping(path = "/statistics/{projectId}")
+    public ResponseEntity<?> getStatisticsCategory(@PathVariable int projectId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Map<String, StatisticsDto> statisticsDtoMap = new HashMap<>();
+        if (authentication == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        } else {
+            List<Task> tasks = taskService.getTasksByProjectId(projectId);
+            Map<String, Map<Integer, List<Long>>> weeklyCompletionTimesMap = new HashMap<>();
+            Map<String, Map<Integer, Integer>> completedTasksPerWeekMap = new HashMap<>();
+            Map<String, Map<Integer, Integer>> receivedTasksPerWeekMap = new HashMap<>();
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            List<Integer> recentWeeks = getRecentWeeks();
+            for (Task t : tasks) {
+                initMapping(t, statisticsDtoMap, weeklyCompletionTimesMap,
+                        completedTasksPerWeekMap, receivedTasksPerWeekMap);
+                StatisticsDto statisticsDto = statisticsDtoMap.get(t.getResponsible().getEmail());
+                Map<Integer, List<Long>> weeklyCompletionTimes = weeklyCompletionTimesMap.get(t.getResponsible().getEmail());
+                Map<Integer, Integer> completedTasksPerWeek = completedTasksPerWeekMap.get(t.getResponsible().getEmail());
+                Map<Integer, Integer> receivedTasksPerWeek = receivedTasksPerWeekMap.get(t.getResponsible().getEmail());
+                statisticsDto.addPriority(t.getPriority());
+                statisticsDto.addCategory(t.getCategory());
+                LocalDateTime deadline = t.getDeadline();
+                LocalDateTime endTime = t.getEndTime();
+                if (endTime != null) {
+                    if (endTime.isAfter(deadline)) {
+                        statisticsDto.addDeadline("OutOfDate");
+                    }
+                    else {
+                        statisticsDto.addDeadline("Completed");
+                    }
+                }
+                else {
+                    if (currentDateTime.isBefore(deadline)) {
+                        statisticsDto.addDeadline("Processing");
+                    }
+                    else {
+                        statisticsDto.addDeadline("ProcessingAndOutOfDate");
+                    }
+                }
+                calculateWeekly(t, recentWeeks, weeklyCompletionTimes,
+                        completedTasksPerWeek, receivedTasksPerWeek);
+            }
+            for (Map.Entry<String, StatisticsDto> entry : statisticsDtoMap.entrySet()) {
+                String email = entry.getKey();
+                StatisticsDto statisticsDto = entry.getValue();
+                Map<Integer, List<Long>> weeklyCompletionTimes = weeklyCompletionTimesMap.get(email);
+                Map<Integer, Integer> completedTasksPerWeek = completedTasksPerWeekMap.get(email);
+                Map<Integer, Integer> receivedTasksPerWeek = receivedTasksPerWeekMap.get(email);
+                Map<Integer, Double> weeklyAverageCompletionTimes = calculateTime(weeklyCompletionTimes);
+                statisticsDto.setWeeklyAverageCompletionTimes(weeklyAverageCompletionTimes);
+                statisticsDto.setCompletedTasksPerWeek(completedTasksPerWeek);
+                statisticsDto.setReceivedTasksPerWeek(receivedTasksPerWeek);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(statisticsDtoMap);
+    }
+
+    public void initMapping(Task t, Map<String, StatisticsDto> statisticsDtoMap,
+                            Map<String, Map<Integer, List<Long>>> weeklyCompletionTimesMap,
+                            Map<String, Map<Integer, Integer>> completedTasksPerWeekMap,
+                            Map<String, Map<Integer, Integer>> receivedTasksPerWeekMap)
+    {
+        if (!statisticsDtoMap.containsKey(t.getResponsible().getEmail())) {
+            StatisticsDto statisticsDto = new StatisticsDto();
+            statisticsDtoMap.put(t.getResponsible().getEmail(), statisticsDto);
+        }
+        if (!weeklyCompletionTimesMap.containsKey(t.getResponsible().getEmail())) {
+            Map<Integer, List<Long>> weeklyCompletionTimes = new HashMap<>();
+            weeklyCompletionTimesMap.put(t.getResponsible().getEmail(), weeklyCompletionTimes);
+        }
+        if (!completedTasksPerWeekMap.containsKey(t.getResponsible().getEmail())) {
+            Map<Integer, Integer> completedTasksPerWeek = new HashMap<>();
+            completedTasksPerWeekMap.put(t.getResponsible().getEmail(), completedTasksPerWeek);
+        }
+        if (!receivedTasksPerWeekMap.containsKey(t.getResponsible().getEmail())) {
+            Map<Integer, Integer> receivedTasksPerWeek = new HashMap<>();
+            receivedTasksPerWeekMap.put(t.getResponsible().getEmail(), receivedTasksPerWeek);
+        }
+    }
 
     @GetMapping(path = "/statistics")
     public ResponseEntity<?> getStatisticsCategory() {
@@ -66,8 +148,8 @@ public class StatisticsController {
                         statisticsDto.addDeadline("ProcessingAndOutOfDate");
                     }
                 }
-
-                calculateWeekly(t, recentWeeks, weeklyCompletionTimes, completedTasksPerWeek, receivedTasksPerWeek);
+                calculateWeekly(t, recentWeeks, weeklyCompletionTimes,
+                                    completedTasksPerWeek, receivedTasksPerWeek);
             }
             Map<Integer, Double> weeklyAverageCompletionTimes = calculateTime(weeklyCompletionTimes);
             statisticsDto.setWeeklyAverageCompletionTimes(weeklyAverageCompletionTimes);
